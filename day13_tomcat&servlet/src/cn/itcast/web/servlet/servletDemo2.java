0da4e2a31449e162ea1c81e61759bed9237f6871
fobird <416575015@qq.com>
2021-04-11 23:46:17 +0800

    增加了输出语句

---
+++ day13_tomcat&servlet/src/cn/itcast/web/servlet/servletDemo2.java
@@ -0,0 +1,35 @@
+package cn.itcast.web.servlet;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+/**
+ * @author superLin
+ * @date 2021-04-11 23:42
+ */
+public class servletDemo2 implements Servlet {
+    @Override
+    public void init(ServletConfig servletConfig) throws ServletException {
+        System.out.println("init......");
+    }
+
+    @Override
+    public ServletConfig getServletConfig() {
+        return null;
+    }
+
+    @Override
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+        System.out.println("service.......");
+    }
+
+    @Override
+    public String getServletInfo() {
+        return null;
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println("destroy....");
+    }
+}
