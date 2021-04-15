dcfb119e3ede2f18177022c0ea53c81639371a0c
fobird <416575015@qq.com>
2021-04-15 15:25:05 +0800

    增加了请求转发项目

---
+++ day14_request/src/cn/itcast/web/servlet/ServletDemo8.java
@@ -0,0 +1,29 @@
+package cn.itcast.web.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author superLin
+ * @date 2021-04-15 12:03
+ */
+@WebServlet("/servletDemo8")
+public class ServletDemo8 extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("demo8被访问了88888888888888888888");
+/*      RequestDispatcher requestDispatcher = request.getRequestDispatcher("/servletDemo9");
+        requestDispatcher.forward(request,response);*/
+        request.getRequestDispatcher("/servletDemo9").forward(request,response);
+        // 只能访问项目内资源
+        // request.getRequestDispatcher("http://www.shuquge.com/txt/74138/13780575.html").forward(request,response);
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        this.doPost(request, response);
+    }
+}
---
+++ day14_request/src/cn/itcast/web/servlet/ServletDemo9.java
@@ -0,0 +1,23 @@
+package cn.itcast.web.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author superLin
+ * @date 2021-04-15 12:03
+ */
+@WebServlet("/servletDemo9")
+public class ServletDemo9 extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("demo9被访问了99999999999999999999");
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        this.doPost(request, response);
+    }
+}
